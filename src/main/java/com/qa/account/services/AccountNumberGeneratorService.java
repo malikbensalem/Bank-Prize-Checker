0e9237dccd911113337c555170a17b491124ca43
malikbensalem <60427086+malikbensalem@users.noreply.github.com>
2020-12-10 21:47:17 +0000

    fix

--- src/main/java/com/qa/account/services/AccountNumberGeneratorService.java
+++ src/main/java/com/qa/account/services/AccountNumberGeneratorService.java
@@ -5,12 +5,10 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class AccountNumberGeneratorService {
 	public String generatorAccountNumber() {
-		// while (getAccounts().equals(this.accountNumber)||this.accountNumber.length()==0) {//how do i get acc nums
 		String accountNumber = "";
 		accountNumber += (char) ThreadLocalRandom.current().nextInt(97, 101);
 		for (int i = 0; i < new int[] { 6, 8, 10 }[new Random().nextInt(3)]; i++) {
 			accountNumber += new Random().nextInt(10);
-			// }
 		}
 		return accountNumber;
 	}
